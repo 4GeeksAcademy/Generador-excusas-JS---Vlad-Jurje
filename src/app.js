ba005a014717dc4ba378e00502f3c0f1194373c3
pacom4n <pacom4n90@gmail.com>
2025-07-18 18:54:05 +0000

    Excusas JV

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['My dog ', 'Ana ', 'My cat ', 'Dad ', 'Somebody ']
+let action = ['ate ', 'broke ', 'stole ', 'peed ',' crushed ']
+let what = ['my homework ', 'my laptop ', 'the money ', ' the blanket ']
+let when = ['at breakfast time.', 'during class.', 'when I was sleeping.', 'while I was playing.']
+
+function getRandomNumber (a, b) {
+return Math.floor(Math.random() * (b - a) + a); 
+}
+
+function getRandom(anyArray){
+let max = anyArray.length;
+let min = 0;
+let random = getRandomNumber (min, max);
+return anyArray[random]; 
+}
+
+
+document.getElementById("excuse").innerHTML = getRandom(who) + getRandom(action) + getRandom(what) + getRandom(when)
 };
+
+
